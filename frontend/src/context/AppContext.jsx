85d14b21659edea5414bf2cb5ddfb050a3ef31fb
Mir Arfan Uddin <75514380+MirArfan@users.noreply.github.com>
2026-01-11 11:14:50 +0600

    Update backend URL in AppContext

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -9,7 +9,7 @@ export const  AppContext=createContext();
 const AppContextProvider=(props)=>{
 
     const currencySymbol='৳'
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const backendUrl = "https://juris-meet.onrender.com"
     const [lawyers, setLawyers] = useState([])
     const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : false)
     const [userData, setUserData]=useState(false)
